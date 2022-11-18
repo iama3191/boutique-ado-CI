4249c60e870296c12fdea5872f709b2cd7064366
iama3191 <ire.andreina.z@gmail.com>
2022-11-18 10:34:15 +0000

    Fix module error, contexts.py file has a typo

--- checkout/views.py
+++ checkout/views.py
@@ -10,11 +10,10 @@ def checkout(request):
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
 
-
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'ordor_form': order_form,
+        'order_form': order_form,
     }
 
     return render(request, template, context)
